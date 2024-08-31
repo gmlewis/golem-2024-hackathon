b104c1b141939012bc947aaa846d68d52b7230fd
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-08-31 06:36:50 -0400

    Update scripts
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gmlewis/golem-2024-hackathon
+
+go 1.23.0
---
+++ scripts/update-profile-picture.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+REPO_DIR=$(realpath ${SCRIPT_DIR}/..)
+source .env.development.local
+
+go run cmd/update-profile-picture/main.go "$@"
