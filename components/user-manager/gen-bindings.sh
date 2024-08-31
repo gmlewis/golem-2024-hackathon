d1a7ad24c85274061f484d3f4fef9d6a175ce2a8
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-08-31 09:26:48 -0400

    About to add timestamps
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- components/user-manager/gen-bindings.sh
+++ components/user-manager/gen-bindings.sh
@@ -2,6 +2,14 @@
 wit-bindgen moonbit wit --derive-show --derive-eq --out-dir .
 mv interface/exports/golem/component/api/*.mbt .
 mv stub.mbt user-manager.mbt
+
+cat <EOF >> user-manager.mbt
+/// The `user-manager` component handles all user authentication, profiles, and info.
+/// Each user is created with a globally-unique XID such that in the future, it will
+/// be possible to change a user's public "@handle" easily (if the new handle is available)
+/// without the need for anything else changing in the backend.
+EOF
+
 mv gen/* .
 sed -i '' -e 's|@api\.||g' interface_exports_golem_component_api_export.mbt
 sed -i '' -e 's|import.*$|import": ["gmlewis/golem-2024-hackathon/components/user-manager/ffi"]|' moon.pkg.json
