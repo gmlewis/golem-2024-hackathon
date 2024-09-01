1f189306405ebc4e36a5b8d8c195991e7935f49c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-09-01 04:17:17 -0400

    Add golem API to user-manager component
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- components/user-manager/gen-bindings.sh
+++ components/user-manager/gen-bindings.sh
@@ -6,5 +6,13 @@ rm stub.mbt
 mv gen/* .
 sed -i '' -e 's|@api\.||g' interface_exports_golem_component_api_export.mbt
 sed -i '' -e 's|import.*$|import": ["gmlewis/golem-2024-hackathon/components/user-manager/ffi"]|' moon.pkg.json
-rm -rf gen worlds interface moon.mod.json
+
+rm -rf gen worlds interface/exports interface/imports/wasi moon.mod.json
+
+rep 'golem/component/ffi' 'gmlewis/golem-2024-hackathon/components/user-manager/ffi' `rg -l golem/component/ffi | grep json`
+
+rep 'golem/component/interface/imports/wasi' 'gmlewis/spin-moonbit-sdk/interface/imports/wasi' `rg -l golem/component/interface/imports/wasi | grep json`
+
+rep 'golem/component/interface/imports/golem' 'gmlewis/golem-2024-hackathon/components/user-manager/interface/imports/golem'  `rg -l golem/component/interface/imports/golem | grep json`
+
 moon fmt
---
+++ components/user-manager/interface/imports/golem/api/saveSnapshot/README.md
@@ -0,0 +1,2 @@
+Interface providing user-defined snapshotting capability. This can be used to perform manual update of workers
+when the new component incompatible with the old one.
