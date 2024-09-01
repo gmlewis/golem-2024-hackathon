b9de998edee81896af27fc55ad0736df8e8fac8e
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-09-01 03:35:55 -0400

    About to change following-result
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scripts/register-new-user.sh
+++ scripts/register-new-user.sh
@@ -8,11 +8,17 @@ go run ${REPO_DIR}/cmd/register-new-user/main.go "$@"
 
 # For Demo:
 # go run ${REPO_DIR}/cmd/register-new-user/main.go -debug -handle "user-1" -password "${GOLEM_2024_HACKATHON_USER1_PASSWORD}"
+# sleep 1
 # go run ${REPO_DIR}/cmd/register-new-user/main.go -debug -handle "user-2" -password "${GOLEM_2024_HACKATHON_USER2_PASSWORD}"
+# sleep 1
 # go run ${REPO_DIR}/cmd/register-new-user/main.go -debug -handle "user-3" -password "${GOLEM_2024_HACKATHON_USER3_PASSWORD}"
+# sleep 1
 # go run ${REPO_DIR}/cmd/register-new-user/main.go -debug -handle "user-4" -password "${GOLEM_2024_HACKATHON_USER4_PASSWORD}"
+# sleep 1
 # go run ${REPO_DIR}/cmd/register-new-user/main.go -debug -handle "user-5" -password "${GOLEM_2024_HACKATHON_USER5_PASSWORD}"
+# sleep 1
 # go run ${REPO_DIR}/cmd/register-new-user/main.go -debug -handle "user-6" -password "${GOLEM_2024_HACKATHON_USER6_PASSWORD}"
+# sleep 1
 # go run ${REPO_DIR}/cmd/register-new-user/main.go -debug -handle "user-7" -password "${GOLEM_2024_HACKATHON_USER7_PASSWORD}"
 
 # For Demo: manually create each new user worker since I couldn't figure out how to do that from the MoonBit wasm plugin.
--- scripts/update-profile-picture.sh
+++ scripts/update-profile-picture.sh
@@ -4,7 +4,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 REPO_DIR=$(realpath ${SCRIPT_DIR}/..)
 source ${REPO_DIR}/.env.development.local
 
-# go run ${REPO_DIR}/cmd/update-profile-picture/main.go "$@"
+go run ${REPO_DIR}/cmd/update-profile-picture/main.go "$@"
 
 # For Demo:
-go run ${REPO_DIR}/cmd/update-profile-picture/main.go -user 'user-1' -xid "${GOLEM_2024_HACKATHON_USER1_XID}" -filename ${HOME}/Pictures/gmlewis-github-profile-photo.jpg
+# go run ${REPO_DIR}/cmd/update-profile-picture/main.go -user 'user-1' -xid "${GOLEM_2024_HACKATHON_USER1_XID}" -filename ${HOME}/Pictures/gmlewis-github-profile-photo.jpg
+# go run ${REPO_DIR}/cmd/get-user-info/main.go -debug -user "user-1" -xid "${GOLEM_2024_HACKATHON_USER1_XID}" -handle "user-1"
