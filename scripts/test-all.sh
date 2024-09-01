5a13ff56b5f1da9e190bbb3f7bc5d1e285557533
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-09-01 12:29:07 -0400

    Clean up warnings
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -4,5 +4,10 @@ REPO_DIR=$(realpath ${SCRIPT_DIR}/..)
 
 cd ${REPO_DIR}
 go test ./...
+pushd components/user
 moon fmt
-moon test -v
+moon test -v --target wasm
+cd ../user-manager
+moon fmt
+moon test -v --target wasm
+popd
